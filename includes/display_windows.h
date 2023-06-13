513167c117aed745d300bd3fc335228d6d04e0a7
Jacob Baumel <jacob.a.baumel@gmail.com>
2023-06-13 13:42:26 -0500

    re-added removed headers

---
+++ includes/display_windows.h
@@ -0,0 +1,33 @@
+#ifndef DISPLAY_WINDOWS_H
+#define DISPLAY_WINDOWS_H
+#include "jsonstuff.h"
+#include "db_functions.h"
+#include "imgui.h"
+#include <vector>
+
+namespace tasker {
+    enum class DisplayWindowStage {
+        pick_workspace,
+        workspace_main,
+    };
+
+    struct connection_add_statics {
+        std::vector<std::string>* connection_options = NULL;
+        const char* current = NULL;
+        char workspace_name[64] = "";
+        tasker::return_code success1 = tasker::return_code::None;
+        char schema[128] = "";
+        tasker::return_code success2 = tasker::return_code::None;
+        char ip[128] = "";
+        int port = 3306;
+        char username[128] = "";
+        bool show_pass = false;
+        char password[128] = "";
+        tasker::return_code success3 = tasker::return_code::None;
+    };
+};
+
+void display_workspace(tasker::json_database& connection, tasker::DisplayWindowStage& stage, int& latestId, bool& refresh, tasker::workspace& config, time_t& timer);
+void display_worskapce_selection(tasker::json_database& connection, tasker::DisplayWindowStage& stage, int& latestId, bool& refresh, tasker::database_array& connections, bool& add_connection, tasker::connection_add_statics& statics);
+
+#endif
---
+++ includes/jsonstuff.h
@@ -0,0 +1,40 @@
+#ifndef JSON_STUFF_H
+#define JSON_STUFF_H
+
+#include <iostream>
+#include <vector>
+
+
+namespace tasker {
+
+    struct json_sql_connection {
+        std::string ip;
+        int port;
+        std::string username;
+        std::string password;
+
+        bool operator==(const json_sql_connection& other);
+    };
+
+    struct json_database {
+        json_sql_connection connection;
+        std::string schema;
+    };
+
+    struct database_array {
+        std::vector<json_database> databases;
+        std::vector<json_sql_connection> connections;
+        json_database& get_database(int index) {return databases.at(index);}
+        json_sql_connection& get_connection(int index) {return connections.at(index);}
+    };
+
+    int add_json_connection(const json_sql_connection& conn);
+    void add_json_database(const tasker::json_database& database);
+
+    void remove_json_connection(const json_sql_connection& conn);
+    void remove_json_database(const json_database& database);
+
+    void get_databases(tasker::database_array& array);
+}
+
+#endif
